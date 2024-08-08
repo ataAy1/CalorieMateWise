4c8be0dbd960ecf1b977f5e084cb0d743cfbb86e
ataAy1 <moon_1905_1907@hotmail.com>
2024-08-08 16:26:21 +0300

    in development ProfileFragment

--- core/data/src/main/java/com/app/data/model/FoodModel.kt
+++ core/data/src/main/java/com/app/data/model/FoodModel.kt
@@ -1,7 +1,10 @@
 package com.app.core.data.model
 
 import com.google.firebase.firestore.DocumentId
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
 data class FoodModel(
     @DocumentId val id: String? = null,
@@ -17,3 +20,20 @@ data class FoodModel(
     val dayOfMonth: String = "",
     val dayName: String = ""
 ) : Serializable
+
+
+@Parcelize
+data class FoodModelParcelize(
+    @DocumentId val id: String? = null,
+    val label: String = "",
+    val calories: Double = 0.0,
+    val protein: Double = 0.0,
+    val fat: Double = 0.0,
+    val carbohydrates: Double = 0.0,
+    val image: String = "",
+    val date: String = "",
+    val year: String = "",
+    val yearOfMonth: String = "",
+    val dayOfMonth: String = "",
+    val dayName: String = ""
+) : Parcelable
